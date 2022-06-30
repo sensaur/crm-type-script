f93d1227f11573faf22d8e8edc6cabfd0f3edf90
Ilya Zhigarev <zhigarev@bk.ru>
2022-06-30 08:02:27 +0300

    react router dom added

---
+++ src/Components/Test1/Test1.tsx
@@ -0,0 +1,7 @@
+function Test2() {
+  return (
+    <div>Hello T1</div>
+  );
+}
+
+export default Test2;
---
+++ src/Components/Test2/Test2.tsx
@@ -0,0 +1,7 @@
+function Test2() {
+  return (
+    <div>Hello T2</div>
+  );
+}
+
+export default Test2;
