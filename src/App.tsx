db427817944b61f0c96291edf6e2f8b9e7a2ab83
Ilya Zhigarev <zhigarev@bk.ru>
2022-06-30 07:56:31 +0300

    react router dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import Test1 from './Components/Test1/Test1';
+import Test2 from './Components/Test2/Test2';
 
 function App() {
   return (
-    <Login />
+    <>
+      <Login />
+      <Routes>
+        <Route path="/test1" element={<Test1 />} />
+        <Route path="/test2" element={<Test2 />} />
+      </Routes>
+    </>
   );
 }
 
