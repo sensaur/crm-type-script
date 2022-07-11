100a5659cdc4027924379e1464776d94ee63cd99
Ilya Zhigarev <zhigarev@bk.ru>
2022-07-11 16:26:33 +0300

    button logout added to header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -140,7 +140,7 @@ function Header() {
           </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <button className="btn btn-outline-success" type="submit" onClick={logout}>Выйти</button>
           </form>
         </div>
       </div>
