ce025bf2314158709fbf7a25644540b7679574b9
Ilya Zhigarev <zhigarev@bk.ru>
2022-07-13 11:30:14 +0300

    badge pill style added

--- src/Components/FranchApps/FranchAppList.tsx
+++ src/Components/FranchApps/FranchAppList.tsx
@@ -51,90 +51,87 @@ const FRANCHAISE_STATUS_CLASS = {
 };
 
 function FranchAppList(props: any) {
-  // console.log('props=>>', props);
   const { items } = props;
-  // console.log('items=>>', items);
   return (
-    <table className="table table-striped table-hover small">
-      <thead>
-        <tr>
-          <th className="text-center">*</th>
-          <th className="text-center">Номер</th>
-          <th className="text-center">Дата</th>
-          <th className="text-center">Статус</th>
-          <th className="text-center">Источник</th>
-          <th className="text-center">Заявитель</th>
-          <th className="text-center">Опыт</th>
-          <th className="text-center">Офис</th>
-          <th className="text-center">Страна</th>
-          <th className="text-center">Менеджер</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items?.length ? items.map((item: any) => (
-          <tr key={item.id}>
-            <td className="text-center tableItem">
-              <input
-                type="checkbox"
-                id={`application-${item.id}`}
-              />
-            </td>
-            <td className="text-center tableItem">
-              {/* <Link to={`/franchising/${item.id}`} target="_blank" itemID={item.id}> */}
-              {item.id}
-              {/* </Link> */}
-            </td>
-            <td className="text-center tableItem">
-              {/* <Link to={`/franchising/${item.id}`} target="_blank"> */}
-              {/*   <Link to={`/franchising/${item.id}`}> */}
-              {moment(item.created).format('DD.MM.YY HH:mm')}
-              {/* </Link> */}
-            </td>
-            <td className="text-center tableItem">
-              {Object.prototype.hasOwnProperty.call(FRANCHAISE_STATUS, item.status) ? (
-                <span className={`badge badge-pill ${FRANCHAISE_STATUS_CLASS[item.status as keyof typeof FRANCHAISE_STATUS_CLASS]}`}>
-                  {FRANCHAISE_STATUS[item.status as keyof typeof FRANCHAISE_STATUS]}
-                </span>
-              )
-                : <span className="badge badge-default badge-pill">не задан</span>}
-            </td>
-            <td className="text-center tableItem">
-              {Object.prototype.hasOwnProperty.call(FRANCHAISE_SOURCE, item.source)
-                ? <span>{FRANCHAISE_SOURCE[item.source as keyof typeof FRANCHAISE_SOURCE]}</span>
-                : <span className="badge badge-default badge-pill">не задан</span>}
-            </td>
-            <td className="text-center tableItem">
-              <div>
-                {item.applicant.name}
-              </div>
-              (
-              {item.applicant.email}
-              ,
-              {' '}
-              {item.applicant.phone}
-              )
-            </td>
-            <td className="text-center tableItem">
-              {item.applicant.have_tourism_experience ? <span>да</span> : <span>нет</span>}
-            </td>
-            <td className="text-center tableItem">
-              {item.office.address}
-              {' '}
-              {' '}
-              (
-              {item.office.city.name}
-              )
-            </td>
-            <td className="text-center tableItem">
-              {item.applicant.country}
-            </td>
-            <td className="text-center tableItem">
-              {item.manager ? `${item.manager.last_name} ${item.manager.first_name}` : ''}
-            </td>
+    <div style={{ width: '100%' }}>
+      <table className="table table-striped table-hover table-sm">
+        <thead>
+          <tr>
+            <th className="text-center">*</th>
+            <th className="text-center">Номер</th>
+            <th className="text-center">Дата</th>
+            <th className="text-center">Статус</th>
+            <th className="text-center">Источник</th>
+            <th className="text-center">Заявитель</th>
+            <th className="text-center">Опыт</th>
+            <th className="text-center">Офис</th>
+            <th className="text-center">Страна</th>
+            <th className="text-center">Менеджер</th>
           </tr>
-        )) : null}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {items?.length ? items.map((item: any) => (
+            <tr key={item.id}>
+              <td className="text-center tableItem">
+                <input
+                  type="checkbox"
+                  id={`application-${item.id}`}
+                />
+              </td>
+              <td className="text-center tableItem">
+                {/* <Link to={`/franchising/${item.id}`} target="_blank" itemID={item.id}> */}
+                {item.id}
+                {/* </Link> */}
+              </td>
+              <td className="text-center tableItem">
+                {/* <Link to={`/franchising/${item.id}`} target="_blank"> */}
+                {/*   <Link to={`/franchising/${item.id}`}> */}
+                {moment(item.created).format('DD.MM.YY HH:mm')}
+                {/* </Link> */}
+              </td>
+              <td className="text-center tableItem">
+                {Object.prototype.hasOwnProperty.call(FRANCHAISE_STATUS, item.status) ? (
+                  <span
+                    className={`badge badge-pill ${FRANCHAISE_STATUS_CLASS[item.status as keyof typeof FRANCHAISE_STATUS_CLASS]}`}
+                  >
+                    {FRANCHAISE_STATUS[item.status as keyof typeof FRANCHAISE_STATUS]}
+                  </span>
+                )
+                  : <span className="badge badge-default badge-pill">не задан</span>}
+              </td>
+              <td className="text-center tableItem">
+                {Object.prototype.hasOwnProperty.call(FRANCHAISE_SOURCE, item.source)
+                  ? <span>{FRANCHAISE_SOURCE[item.source as keyof typeof FRANCHAISE_SOURCE]}</span>
+                  : <span className="badge badge-default badge-pill">не задан</span>}
+              </td>
+              <td className="text-center tableItem">
+                <div>
+                  {item.applicant.name}
+                </div>
+                {/* {item.applicant.email} */}
+                {/* , */}
+                {/* &nbsp; */}
+                {/* {item.applicant.phone} */}
+              </td>
+              <td className="text-center tableItem">
+                {item.applicant.have_tourism_experience ? <span>да</span> : <span>нет</span>}
+              </td>
+              <td className="text-center tableItem">
+                {item.office.address}
+                {/* &nbsp; */}
+                {/* {item.office.city.name} */}
+              </td>
+              <td className="text-center tableItem">
+                {item.applicant.country}
+              </td>
+              <td className="text-center tableItem">
+                {item.manager ? `${item.manager.last_name} ${item.manager.first_name}` : ''}
+              </td>
+            </tr>
+          )) : null}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
